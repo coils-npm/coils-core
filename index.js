62caa41cc14653f4fd3b762431648b99038d73da
mars <mars@mars.com>
2018-09-06 22:25:55 +0800

    first init CoilsCore

---
+++ index.js
@@ -0,0 +1,33 @@
+class Coils {
+	constructor (options) {
+		for (let key in options) {
+			const freezeObj = Object.freeze(options[key])
+			Object.defineProperties(this, { [key]: { "get": () => { return freezeObj } } })
+		}
+	}
+	
+	use (module) {
+		if (module.mounted) {
+			return module.mounted(this)
+		} else {
+			console.error(`module ${module} must define mounted method`)
+		}
+	}
+	
+	setGlobalVars () {
+		Object.defineProperties(global, { "$application": { "get": () => { return application } } })
+		for (let model of this['$Models']) {
+			Object.defineProperties(global, {
+				[model.name]: { "get": () => { return model } }
+		})
+		}
+	}
+	
+	startKoa (port) {
+		port = port || this.envConfig.PORT || 3000
+		this.$koa.listen(port, function () {
+			console.log(`app start at: http://localhost:${port}`)
+		})
+	}
+}
+module.exports = Coils
